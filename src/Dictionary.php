2aa53fe69dbdf57fb225d71bdb9bbbab5261ebb3
Sakurai <sakurai.evsa@gmail.com>
2023-11-07 19:41:04 +0700

    WIP: Add Missing Dictionary.php

---
+++ src/Dictionary.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace shyevsa\security;
+
+class Dictionary
+{
+    public const POL_SELF = "'self'";
+    public const POL_UNSAFE_INLINE = "'unsafe-inline'";
+    public const POL_NONE = "'none'";
+
+    public const POL_UNSAFE_EVAL = "'unsafe-eval'";
+}
